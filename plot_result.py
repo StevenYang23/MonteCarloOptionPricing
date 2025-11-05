b1984f0fb832d3234d4385df7387b4ee7dc7497a
Dingjingmu Yang <stevenydjm@gmail.com>
2025-11-05 17:08:00 +0800

    update

--- plot_result.py
+++ plot_result.py
@@ -50,7 +50,7 @@ def plot_v_paths(V_path_dupire,V_path_Heston,V_path_Garch,V_path_GBM):
     axs[1, 1].legend()
     plt.tight_layout()
     plt.show()
-def plot_payouts(local_vol_payouts, heston_payouts, Garch_Vol_payouts, Constant_vol_payouts):
+def plot_payouts(local_vol_payouts, heston_payouts, Garch_Vol_payouts, Constant_vol_payouts,title):
     fig, axs = plt.subplots(2, 2, figsize=(12, 8))
     axs = axs.ravel()
     distributions = [
@@ -83,5 +83,5 @@ def plot_payouts(local_vol_payouts, heston_payouts, Garch_Vol_payouts, Constant_
         ax.set_xlabel("Payout")
         ax.set_ylabel("Density")
         ax.legend()
-    fig.suptitle('Histogram of Option Payouts with LogNorm Fit')
+    fig.suptitle(title)
     plt.tight_layout(rect=[0, 0.03, 1, 0.95])
